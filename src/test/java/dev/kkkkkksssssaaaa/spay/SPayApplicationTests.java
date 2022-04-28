f0a00c65edd6b9095912daf761bc64b9f5d804eb
kkkkkksssssaaaa <kofc312@naver.com>
2022-04-28 23:07:50 +0900

    feat: 새 프로젝트 생성

---
+++ src/test/java/dev/kkkkkksssssaaaa/spay/SPayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kkkkkksssssaaaa.spay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
