fa6c1c9486e005907dd55d80f07548cdcf9e107c
kkkkkksssssaaaa <kofc312@naver.com>
2022-05-19 23:54:38 +0900

    feat: 사용자 전화번호 모델 Phone 추가

---
+++ src/main/java/dev/kkkkkksssssaaaa/spay/user/Phone.java
@@ -0,0 +1,54 @@
+package dev.kkkkkksssssaaaa.spay.user;
+
+import dev.kkkkkksssssaaaa.spay.util.Strings;
+
+import java.util.regex.Pattern;
+
+public class Phone {
+
+    private static final int LENGTH = 13;
+    private static final String REGEX = "^(010)-([\\d]{4})-([\\d]{4})$";
+
+    private final String phone;
+
+    private Phone(String phone) {
+        validate(phone);
+
+        this.phone = phone;
+    }
+
+    public static Phone of(String phone) {
+        return new Phone(phone);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!o.getClass().equals(this.getClass())) {
+            return false;
+        }
+
+        return ((Phone) o).phone.equals(this.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.phone.hashCode();
+    }
+
+    private void validate(String phone) {
+        if (Strings.isEmpty((phone))) {
+            // TODO 커스텀 예외 정의
+            throw new IllegalArgumentException();
+        }
+
+        if (notMatch(phone)) {
+            // TODO 커스텀 예외 정의
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean notMatch(String name) {
+        return !Pattern.matches(REGEX, name);
+    }
+
+}
