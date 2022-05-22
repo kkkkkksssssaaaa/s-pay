c58ff82df048caa05bbc9b067b0c35436beb73b5
kkkkkksssssaaaa <kofc312@naver.com>
2022-05-23 00:16:55 +0900

    docs(Privacy): 타입 주석 추가

--- src/main/java/dev/kkkkkksssssaaaa/spay/user/privacy/Privacy.java
+++ src/main/java/dev/kkkkkksssssaaaa/spay/user/privacy/Privacy.java
@@ -2,6 +2,11 @@ package dev.kkkkkksssssaaaa.spay.user.privacy;
 
 import java.util.Objects;
 
+/**
+ * 사용자의 개인 정보에 대한 공개 클래스
+ * 해당 클래스의 필드로 사용자의 실제 정보에 대한 private 항목을 가지고 있으며
+ * 위 항목들은 해당 클래스를 통해 외부에서 접근할 수 있음
+ * */
 public class Privacy {
 
     private final Name name;
