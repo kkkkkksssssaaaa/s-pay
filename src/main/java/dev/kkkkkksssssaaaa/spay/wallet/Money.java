fb0d9c6868af154145e5ca844c82e75546c45674
kkkkkksssssaaaa <kofc312@naver.com>
2022-05-24 22:49:53 +0900

    feat(Money): 최소/최대 보유 금액 검증 로직 추가

--- src/main/java/dev/kkkkkksssssaaaa/spay/wallet/Money.java
+++ src/main/java/dev/kkkkkksssssaaaa/spay/wallet/Money.java
@@ -5,22 +5,45 @@ package dev.kkkkkksssssaaaa.spay.wallet;
  * */
 class Money {
 
-    private int money;
+    private static final int ZERO = 0;
+    private static final int FULL = 1000000000;
 
-    private Money(int money) {
-        this.money = money;
-    }
-
-    protected static Money of(int money) {
-        return new Money(money);
-    }
+    private int money = 0;
 
     protected void increase(Money money) {
+        checkFull(money);
+
         this.money += money.money;
     }
 
     protected void shrink(Money money) {
+        checkZero(money);
+
         this.money -= money.money;
+    }
+
+    private void checkFull(Money money) {
+        if (money.money > FULL) {
+            throw new IllegalArgumentException(
+                    String.format("최대 보유 가능 금액은 %d원 입니다.", FULL));
+        }
+
+        if ((this.money + money.money) > FULL) {
+            throw new IllegalArgumentException(
+                    String.format("최대 보유 가능 금액은 %d원 입니다.", FULL));
+        }
+    }
+
+    private void checkZero(Money money) {
+        if (this.money < money.money) {
+            throw new IllegalArgumentException(
+                    "소비하려는 금액이 현재 금액보다 더 클 수 없습니다.");
+        }
+
+        if (money.money < 0) {
+            throw new IllegalArgumentException(
+                    String.format("최소 보유 가능 금액은 %d원 입니다.",  ZERO));
+        }
     }
 
 }
