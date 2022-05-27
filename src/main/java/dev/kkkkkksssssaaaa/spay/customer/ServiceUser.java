170b5328acb5e81c43ae87c0c75173bba39fe6af
kkkkkksssssaaaa <kofc312@naver.com>
2022-05-27 22:38:10 +0900

    feat: 서비스 이용 고객 모델 ServiceUser 추가

---
+++ src/main/java/dev/kkkkkksssssaaaa/spay/customer/ServiceUser.java
@@ -0,0 +1,33 @@
+package dev.kkkkkksssssaaaa.spay.customer;
+
+import dev.kkkkkksssssaaaa.spay.user.User;
+import dev.kkkkkksssssaaaa.spay.wallet.Money;
+import dev.kkkkkksssssaaaa.spay.wallet.Wallet;
+
+/**
+ * 서비스를 사용하는 실제 사용자(고객) 모델
+ * */
+public class ServiceUser {
+
+    private final User user;
+    private final Wallet wallet;
+
+    private ServiceUser(User user) {
+        this.user = user;
+        this.wallet = Wallet.newWallet();
+    }
+
+    public void deposit(Money money) {
+        this.wallet.deposit(money);
+    }
+
+    public void withdraw(Money money) {
+        this.wallet.withdraw(money);
+    }
+
+    // TODO 별도의 송금 오브젝트를 생성하여 변경
+    public void wire(ServiceUser to, Money money) {
+        to.deposit(money);
+    }
+
+}
