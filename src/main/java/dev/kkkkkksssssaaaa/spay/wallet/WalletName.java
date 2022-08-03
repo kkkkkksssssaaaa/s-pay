1f544ae714304fd41c68ac1866d436de8ab1112f
kkkkkksssssaaaa <kofc312@naver.com>
2022-08-03 23:03:48 +0900

    feat: 지갑 이름 객체 WalletName 추가

---
+++ src/main/java/dev/kkkkkksssssaaaa/spay/wallet/WalletName.java
@@ -0,0 +1,18 @@
+package dev.kkkkkksssssaaaa.spay.wallet;
+
+import dev.kkkkkksssssaaaa.spay.atomical.AbstractName;
+
+final class WalletName extends AbstractName {
+
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 10;
+    private static final String REGEX = ".*";
+
+    private WalletName(String name) {
+        super(name, REGEX, MIN_LENGTH, MAX_LENGTH);
+    }
+
+    static WalletName of(String name) {
+        return new WalletName(name);
+    }
+}
