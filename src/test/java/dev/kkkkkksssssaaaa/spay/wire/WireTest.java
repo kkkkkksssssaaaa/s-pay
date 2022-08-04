fb330d0b5eb86f0710d7cfed7cef47c820167441
kkkkkksssssaaaa <kofc312@naver.com>
2022-08-04 22:38:48 +0900

    refactor: WireTest 의 일부 메서드 선언 위치 조정

--- src/test/java/dev/kkkkkksssssaaaa/spay/wire/WireTest.java
+++ src/test/java/dev/kkkkkksssssaaaa/spay/wire/WireTest.java
@@ -4,21 +4,38 @@ import dev.kkkkkksssssaaaa.spay.serviceuser.ServiceUser;
 import dev.kkkkkksssssaaaa.spay.user.User;
 import dev.kkkkkksssssaaaa.spay.user.privacy.Privacy;
 import dev.kkkkkksssssaaaa.spay.wallet.Money;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Wire")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class WireTest {
 
-    @Test
-    void 본인에게_송금할_수_없다() {
-        assertThrows(IllegalArgumentException.class,
-                () -> user1().wire(user1(), Money.of(10000)));
+    @Nested
+    @DisplayName("toServiceUser")
+    class ToServiceUserTest {
+
+        @Test
+        void 본인에게_송금할_수_없다() {
+            assertThrows(IllegalArgumentException.class,
+                    () -> user1().wire(user1(), Money.of(10000)));
+        }
+
+        @Test
+        void 본인이_아닌_다른_서비스_유저에게_송금할_수_있다() {
+            assertDoesNotThrow(() -> user1().wire(user2(), Money.of(5000)));
+        }
     }
 
-    @Test
-    void 본인이_아닌_다른_서비스_유저에게_송금할_수_있다() {
-        assertDoesNotThrow(() -> user1().wire(user2(), Money.of(5000)));
+    @Nested
+    @DisplayName("money")
+    class MoneyTest {
+
+        @Test
+        void 송금이_완료되면_잔액을_조회할_수_있다() {
+            assertDoesNotThrow(() -> user1().wire(user2(), Money.of(5000)));
+        }
     }
 
     private ServiceUser user1() {
