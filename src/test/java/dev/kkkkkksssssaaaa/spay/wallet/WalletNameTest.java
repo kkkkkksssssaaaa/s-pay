3553815eb04fd75d32caf1365381edd5f4a204ef
kkkkkksssssaaaa <kofc312@naver.com>
2022-08-04 21:43:33 +0900

    test: WalletNameTest 의 길이 검증 테스트 추가

--- src/test/java/dev/kkkkkksssssaaaa/spay/wallet/WalletNameTest.java
+++ src/test/java/dev/kkkkkksssssaaaa/spay/wallet/WalletNameTest.java
@@ -34,5 +34,24 @@ class WalletNameTest {
             assertThrows(IllegalArgumentException.class, () -> WalletName.of(""));
             assertThrows(IllegalArgumentException.class, () -> WalletName.of(null));
         }
+
+        @Test
+        void 지갑_이름이_열_글자를_넘으면_IllegalArgumentException을_던진다() {
+            assertThrows(IllegalArgumentException.class, () -> WalletName.of("01234567891"));
+        }
+
+        @Test
+        void 지갑_이름이_한_글자_이상이면_예외를_반환하지_않는다() {
+            assertDoesNotThrow(() -> WalletName.of("0"));
+            assertDoesNotThrow(() -> WalletName.of("01"));
+            assertDoesNotThrow(() -> WalletName.of("012"));
+            assertDoesNotThrow(() -> WalletName.of("0123"));
+            assertDoesNotThrow(() -> WalletName.of("01234"));
+            assertDoesNotThrow(() -> WalletName.of("012345"));
+            assertDoesNotThrow(() -> WalletName.of("0123456"));
+            assertDoesNotThrow(() -> WalletName.of("01234567"));
+            assertDoesNotThrow(() -> WalletName.of("012345678"));
+            assertDoesNotThrow(() -> WalletName.of("0123456789"));
+        }
     }
 }
