6ac72e49e235669ca9fca5a5a5cd2f5171a26f4a
kkkkkksssssaaaa <kofc312@naver.com>
2025-03-06 23:46:32 +0900

    feat(exchange): add field to ExchangeRateMap

--- exchange/src/main/kotlin/dev/kkkkkksssssaaaa/spay/exchange/domain/exchangerate/ExchangeRateMap.kt
+++ exchange/src/main/kotlin/dev/kkkkkksssssaaaa/spay/exchange/domain/exchangerate/ExchangeRateMap.kt
@@ -4,6 +4,7 @@ import dev.kkkkkksssssaaaa.spay.exchange.domain.currency.CurrencyType
 
 data class ExchangeRateMap(
     val baseAmount: Long,
+    val baseCurrencyType: CurrencyType,
     private val map: Map<CurrencyType, Double>
 ) {
     fun find(targetCurrencyType: CurrencyType): Double {
