9f0576c6a4c7dda5319851411591a8da585ae8ed
kkkkkksssssaaaa <kofc312@naver.com>
2025-03-12 00:14:06 +0900

    feat(exchange): implement buy fee strategy service

--- exchange/src/main/kotlin/dev/kkkkkksssssaaaa/spay/exchange/domain/fee/CalculateBuyFeeService.kt
+++ exchange/src/main/kotlin/dev/kkkkkksssssaaaa/spay/exchange/domain/fee/CalculateBuyFeeService.kt
@@ -1,10 +1,63 @@
 package dev.kkkkkksssssaaaa.spay.exchange.domain.fee
 
+import dev.kkkkkksssssaaaa.spay.exchange.domain.fee.strategy.FeeStrategyService
+import dev.kkkkkksssssaaaa.spay.exchange.domain.fee.strategy.Strategy
+import dev.kkkkkksssssaaaa.spay.exchange.domain.fee.strategy.StrategyType
+import dev.kkkkkksssssaaaa.spay.exchange.domain.money.Money
 import org.springframework.stereotype.Service
 
 @Service
-class CalculateBuyFeeService: CalculateFeeService {
-    override fun doCalculate(amount: Double): Fee {
-        TODO("Not yet implemented")
+class CalculateBuyFeeService(
+    private val strategy: FeeStrategyService
+): CalculateFeeService() {
+    override fun doCalculate(exchangeMoney: Money): Fee {
+        val strategy = strategy.getBuyStrategy()
+
+        return when (strategy.type) {
+            StrategyType.PERCENTAGE -> {
+                Fee(
+                    strategy = strategy,
+                    amount = calculatePercentageFee(strategy, exchangeMoney)
+                )
+            }
+            StrategyType.STATIC -> {
+                Fee(
+                    strategy = strategy,
+                    amount = calculatedStaticFee(strategy, exchangeMoney)
+                )
+            }
+        }
+    }
+
+    override fun calculatePercentageFee(
+        strategy: Strategy,
+        exchangeMoney: Money
+    ): Money {
+        val feeAmount = exchangeMoney.amount / 100.0 * strategy.value
+
+        return Money(
+            amount = feeAmount,
+            currency = exchangeMoney.currency
+        )
+    }
+
+    override fun calculatedStaticFee(
+        strategy: Strategy,
+        exchangeMoney: Money
+    ): Money {
+        return when (exchangeMoney.amount) {
+            in 100000.0..199999.9 -> Money(
+                amount = 2000.0,
+                currency = exchangeMoney.currency
+            )
+            in 200000.0..499999.9 -> Money(
+                amount = 4000.0,
+                currency = exchangeMoney.currency
+            )
+            else -> Money(
+                amount = 0.0,
+                currency = exchangeMoney.currency
+            )
+        }
     }
 }
--- exchange/src/main/kotlin/dev/kkkkkksssssaaaa/spay/exchange/domain/fee/strategy/StrategyAdapter.kt
+++ exchange/src/main/kotlin/dev/kkkkkksssssaaaa/spay/exchange/domain/fee/strategy/StrategyAdapter.kt
@@ -2,6 +2,7 @@ package dev.kkkkkksssssaaaa.spay.exchange.domain.fee.strategy
 
 import org.springframework.stereotype.Service
 
+// TODO: 통화에 따라 수수료 정책 각기 적용
 interface StrategyAdapter {
     fun getBuyStrategy(): Strategy
     fun getSellStrategy(): Strategy
@@ -12,14 +13,14 @@ internal class StrategyAdapterImpl : StrategyAdapter {
     override fun getBuyStrategy(): Strategy {
         return Strategy(
             type = StrategyType.PERCENTAGE,
-            value = 1.2
+            value = 1.0
         )
     }
 
     override fun getSellStrategy(): Strategy {
         return Strategy(
             type = StrategyType.PERCENTAGE,
-            value = 1.2
+            value = 1.0
         )
     }
 }
