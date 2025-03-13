78bf93566b172c0f653808845f174c948b512522
seunga.kim <seunga.kim@trustay.me>
2025-03-13 14:49:10 +0900

    fix(exchange): change override method to CalculateSellFeeService

--- exchange/src/main/kotlin/dev/kkkkkksssssaaaa/spay/exchange/domain/fee/CalculateSellFeeService.kt
+++ exchange/src/main/kotlin/dev/kkkkkksssssaaaa/spay/exchange/domain/fee/CalculateSellFeeService.kt
@@ -1,10 +1,20 @@
 package dev.kkkkkksssssaaaa.spay.exchange.domain.fee
 
+import dev.kkkkkksssssaaaa.spay.exchange.domain.fee.strategy.Strategy
+import dev.kkkkkksssssaaaa.spay.exchange.domain.money.Money
 import org.springframework.stereotype.Service
 
 @Service
-class CalculateSellFeeService: CalculateFeeService {
-    override fun doCalculate(amount: Long): Fee {
+class CalculateSellFeeService: CalculateFeeService() {
+    override fun doCalculate(exchangeMoney: Money): Fee {
+        TODO("Not yet implemented")
+    }
+
+    override fun calculatePercentageFee(strategy: Strategy, exchangeMoney: Money): Money {
+        TODO("Not yet implemented")
+    }
+
+    override fun calculatedStaticFee(strategy: Strategy, exchangeMoney: Money): Money {
         TODO("Not yet implemented")
     }
 }
