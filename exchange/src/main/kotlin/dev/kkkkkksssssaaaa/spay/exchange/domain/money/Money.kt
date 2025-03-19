8793d597343027836fc51cb23247f3c8fe59021f
kkkkkksssssaaaa <kofc312@naver.com>
2025-03-20 00:06:58 +0900

    feat(exchange): override operator for money object

--- exchange/src/main/kotlin/dev/kkkkkksssssaaaa/spay/exchange/domain/money/Money.kt
+++ exchange/src/main/kotlin/dev/kkkkkksssssaaaa/spay/exchange/domain/money/Money.kt
@@ -6,4 +6,20 @@ class Money(
     val currency: Currency,
     val value: Double
 ) {
+    operator fun times(targetMoney: Money): Money {
+        if (this.currency != targetMoney.currency) {
+            throw UnsupportedOperationException(
+                "Unable to calculate because the currency types do not match."
+            )
+        }
+
+        return Money(
+            this.currency,
+            this.value * targetMoney.value
+        )
+    }
+
+    operator fun compareTo(value: Double): Int {
+        return this.value.compareTo(value)
+    }
 }
