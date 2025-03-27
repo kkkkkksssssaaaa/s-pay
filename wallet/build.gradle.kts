84af270eac53662f5d6b650b403d603f93e73aa8
kkkkkksssssaaaa <kofc312@naver.com>
2025-03-27 14:46:23 +0900

    feat(wallet): initialize project

--- wallet/build.gradle.kts
+++ wallet/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "dev.kkkkkksssssaaaa.spay.exchange"
+group = "dev.kkkkkksssssaaaa.spay.wallet"
 version = "0.0.1-SNAPSHOT"
 
 java {
