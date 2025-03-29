d4ffbd3e2145c2399a051b002bc460bb708b36e6
kkkkkksssssaaaa <kofc312@naver.com>
2025-03-29 23:24:53 +0900

    feat(exchange): add rounding when exchange amount calculated

--- exchange/src/main/kotlin/dev/kkkkkksssssaaaa/spay/exchange/domain/exchange/ExchangeAmountCalculator.kt
+++ exchange/src/main/kotlin/dev/kkkkkksssssaaaa/spay/exchange/domain/exchange/ExchangeAmountCalculator.kt
@@ -2,6 +2,7 @@ package dev.kkkkkksssssaaaa.spay.exchange.domain.exchange
 
 import dev.kkkkkksssssaaaa.spay.exchange.domain.exchangerate.ExchangedRate
 import dev.kkkkkksssssaaaa.spay.exchange.domain.money.Money
+import kotlin.math.round
 
 /**
  * @property exchangeRate 환율
@@ -29,23 +30,25 @@ data class ExchangeResult(
  * 현재 환율에 맞게 환전할 외화를 계산하고,
  * 현재 환율에 맞는 기준 통화를 계산
  *
- * TODO: 환전 결과가 소수점인 경우 반올림 처리
+ * 계산 결과가 소수점이면 반올림 처리
  * */
 class ExchangeAmountCalculator {
     companion object {
         fun doCalculate(
             request: ExchangeCalculateRequest
         ): ExchangeResult {
-            val target = request.targetAmount
-
-            val base = Money(
-                currency = request.exchangeRate.base.currency,
-                value = request.exchangeRate.target.value *
+            val roundedResult = round(
+                request.exchangeRate.target.value *
                     request.targetAmount.value
             )
 
+            val base = Money(
+                currency = request.exchangeRate.base.currency,
+                value = roundedResult
+            )
+
             return ExchangeResult(
-                target = target,
+                target = request.targetAmount,
                 base = base
             )
         }
